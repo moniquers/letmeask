de5efff25da908d431f23ba849a6a862ee7201f4
Monique Ribeiro Sousa <ribeiromonique90@gmail.com>
2021-06-25 14:55:30 -0300

    adiciona funcionalidades remover pergunta e encerrar sala

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,11 +1,14 @@
 import { Button } from "../components/Button";
 import '../styles/room.scss';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import { RoomCode } from '../components/RoomCode';
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
+
+import deleteImg from '../assets/images/delete.svg';
+import { database } from "../services/firebase";
 
 type RoomParams = {
     id: string;
@@ -13,9 +16,24 @@ type RoomParams = {
 
 export function AdminRoom() {
 
+    const history = useHistory();
     const params = useParams<RoomParams>();
     const roomId = params.id;
     const { title, questions } = useRoom(roomId);
+
+    async function handleEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date()
+        });
+
+        history.push("/");
+    }
+
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm("Tem certeza que você deseja excluir esta pergunta?")) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
 
     return (
         <div id="page-room">
@@ -24,7 +42,11 @@ export function AdminRoom() {
                     <img src={logoImg} alt="logo letmeask" />
                     <div>
                         <RoomCode code={roomId} />
-                        <Button isOutlined>Encerrar sala</Button>
+                        <Button
+                            type="button"
+                            onClick={handleEndRoom}
+                            isOutlined
+                        >Encerrar sala</Button>
                     </div>
                 </div>
             </header>
@@ -46,10 +68,11 @@ export function AdminRoom() {
                                     content={question.content}
                                     author={question.author}
                                 >
-                                    {question.likeCount > 0 &&
-                                        <span>{question.likeCount}</span>
-                                    }
-
+                                    <button
+                                        type="button"
+                                        onClick={() => handleDeleteQuestion(question.id)}>
+                                        <img src={deleteImg} alt="Remover pergunta" />
+                                    </button>
                                 </Question>
                             );
                         })
